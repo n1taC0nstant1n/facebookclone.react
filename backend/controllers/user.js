31f6554790b5848e23d4ec5ea91cf304d288a533
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-22 13:45:02 +0300

    20 Setup the User Model

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,7 @@
 exports.home = (req, res) => {
-  res.send("Welcome from user home");
+  //res.send("Welcome from user home");
+  res.status(200).json({
+    message: "Something",
+    error: "Something else",
+  });
 };
